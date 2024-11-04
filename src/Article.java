6471232412bdb831a944737f3f0cb7a5f2d43c81
Kabir Arora <152303949+KabirArora7@users.noreply.github.com>
2024-11-04 11:56:51 -0500

    NewWWW
    NEWWWW

--- src/Article.java
+++ src/Article.java
@@ -4,6 +4,11 @@ import java.util.List;
 public class Article extends ArticleComponent {
     private List<ArticleComponent> components = new ArrayList<>();
 
+    public ArticleIterator iterator(){
+        // returns an instance of ArticleIterator initialized with the list of components in an article
+        return new ArticleIterator(components);
+    }
+
     public void addComponent(ArticleComponent component) {
         components.add(component);
     }
